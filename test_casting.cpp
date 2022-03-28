b113dbf5f641aa1944b8034a2b4dc1b7e6c2126a
Derek <derek987987@hotmail.com>
2022-03-29 02:41:47 +0800

    casting different type of variables

---
+++ test_casting.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using std::cout;
+
+int main(){
+    cout << "Hello " << '\n' << 2+2;
+
+    cout << '\n' << '\n';
+
+    int i;
+    //fractional part will be lost
+    i = 4.9;
+    i = static_cast<int>(4.9);
+    cout << i << '\n';
+
+    auto j = 5;
+    cout << j << '\n';
+
+    auto f = 4.9;
+    j = f;
+    f = static_cast<int>(f);
+    cout << f << '\n';
+    f = 9 / 5;
+    cout << f << '\n';
+    f = 9.0 / 5;
+    cout << f << '\n';
+    
+    return 0;
+}
